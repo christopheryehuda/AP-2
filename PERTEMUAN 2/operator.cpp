51ee6007a8f2101aa6e7aba14ba9841b7914747f
christopheryehuda <oakun8751@gmail.com>
2025-05-25 22:02:37 +0700

    Pertemuan 2 : Penggunaan Operator

---
+++ PERTEMUAN 2/operator.cpp
@@ -0,0 +1,122 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+
+    system("CLS");
+
+    //assigment operator
+    a = 3;
+    b = 5;
+
+
+    /*aritmathical operator
+    int tambah = a+b;
+    int kurang = a-b;
+    int kali = a*b;
+    float bagi = a/b; (float)a / (float)b; //type casting : berguna untuk mengubah tipe data dari sebuah variabel
+    int modulo = a%b;
+
+    cout << "Hasil penjumlahan = " << tambah << endl;
+    cout << "Hasil pengurangan = " << kurang << endl;
+    cout << "Hasil perkalian = " << kali << endl;
+    cout << "Hasil pembagian = " << bagi << endl;
+    cout << "Hasil sisa bagi = " << modulo << endl;
+    return 0;
+    */
+    
+    //relationall operator
+    //cout << (a==b) << endl;
+    //cout << (a<b) << endl;
+    //cout << (a<=b) << endl;
+    //cout << (a>b) << endl;
+    //cout << (a>=b) << endl;
+    //cout << (a!=b) << endl;
+
+    // Logigal operator
+    //cout << (true && true) << endl;
+    //cout << (true && false) << endl;
+    //cout << (false && true) << endl;
+    //cout << (false && false) << endl;
+  
+    //cout << (true || true) << endl;
+    //cout << (true || false) << endl;
+    //cout << (false || true) << endl;
+    //cout << (false || false) << endl;
+
+    //cout << !true << endl;
+    //cout << !flase << endl;
+
+    //Bitwise operator (&, |, ^, ~, >>, <<) --> biner
+    // cout << (5%7) << endl;
+    // cout << (5|7) << endl;
+    // cout << (5^7) << endl;
+    // cout << (~7) << endl;
+    // cout << (7<<2) << endl;
+    // cout << (7>>2) << endl;
+
+    //Shorthand 
+    // a += 7; //a = a + 7;
+    // cout  << a << endl;
+
+    // a -= 7; //a = a - 7;
+    // cout  << a << endl;
+
+    // a *= 7; //a = a * 7;
+    // cout  << a << endl;
+
+    // a /= 7; //a = a // 7;
+    // cout  << a << endl;
+
+    //Increment, Decrement
+    // Pre Increment ++a 
+    // cout << a << endl;
+    // cout << ++a << endl;
+
+    // cout << b << endl;
+    // cout << ++b << endl;
+
+    Post Increment a++
+    cout << a << endl;
+    cout << a++ << endl;
+    cout << a << endl;
+ 
+    cout << b << endl;
+    cout << b++ << endl;
+    cout << b << endl;
+
+    // //Pre Decrement
+    //   cout << a << endl;
+    //   cout << --a << endl;
+
+    //   cout << b << endl;
+    //   cout << --b << endl;
+
+    //Post Decrement
+    // cout << a << endl;
+    // cout << a-- << endl;
+    // cout << a << endl;
+ 
+    // cout << b << endl;
+    // cout << b-- << endl;
+    // cout << b << endl;
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+} 
